046894c29488ea5f2d625e19e2bb69027bac8181
Julio Renner <juliorenner123@gmail.com>
2017-06-15 16:13:45 -0300

    Update progresso1.c
    [GZIP] Reading from gzip stdout and printing values.

--- antivirus/src/progresso1.c
+++ antivirus/src/progresso1.c
@@ -16,40 +16,44 @@
 
 #define _XOPEN_SOURCE = 700;
 
-char* FILE_NAME = "teste.txt";
+char* FILE_NAME = "teste.txt.gz";
 
 int openFile(char* fileName);
 bool isZippedFile(int fileDescriptor);
+int unzipFile(char* fileName);
+void executeUnzip(char* fileName);
 
 int main(int argc, const char * argv[]) {
-    int fileDescriptor = openFile(FILE_NAME);
+    int fileDescriptorToCheckType = openFile(FILE_NAME);
 
     unsigned int byte[2];
 
-    int count = read(fileDescriptor, &byte, 2);
+    int count = read(fileDescriptorToCheckType, &byte, 2);
 
     if (count <= 0) {
         printf("read failed\n");
         exit(0);
     }
 
+    close(fileDescriptorToCheckType);
+
+    int iFileDescriptor;
     if (isZippedFile(byte[0])){
-        printf("zipped file");
+        printf("zipped file\n");
+        iFileDescriptor = unzipFile(FILE_NAME);
+    } else {
+        iFileDescriptor = openFile(FILE_NAME);
+
+        do{
+            char buf[15];
+            count = read(iFileDescriptor, buf, 14);
+            if (count > 0 ) {
+                //Check for entered string.
+                printf("%s\n", buf);
+            }
+
+        } while( count > 0);
     }
-    //printf("%X\n", byte[0]);
-
-    close(fileDescriptor);
-
-    fileDescriptor = openFile(FILE_NAME);
-    do{
-        char buf[15];
-        count = read(fileDescriptor, buf, 14);
-        if (count > 0 ) {
-            //Check for entered string.
-            printf("%s\n", buf);
-        }
-    
-    } while( count > 0);
 
     return 0;
 }
@@ -59,7 +63,7 @@ int openFile(char* fileName){
     int fileDescriptor = open(fileName, O_CLOEXEC | O_RDONLY);
 
     if (!(fileDescriptor > 0)){
-        perror("Error reading file");
+        perror("Error reading file\n");
         exit(0);
     }
 
@@ -90,4 +94,43 @@ bool isZippedFile(int value) {
 
     return false;
 
+}
+
+int unzipFile(char* fileName){
+    int pfd[2];
+
+    if (pipe(pfd) != 0){
+        perror("error setting pipe\n");
+        exit(EXIT_FAILURE);
+    };
+
+    switch (fork()){
+        case -1:
+            perror("fork error\n");
+            exit(EXIT_FAILURE);
+        case 0:
+            close(pfd[0]);
+            dup2(pfd[1], STDOUT_FILENO);
+            executeUnzip(fileName);
+        default:
+            close(pfd[1]);
+            char buf[15];
+            int count = -1;
+            while (count != 0) {
+                count = read(pfd[0], buf, 14);
+                if (count > 0)
+                    printf("%s\n", buf); }
+            return 0;
+            //read file and search for virus
+    }
+
+}
+
+void executeUnzip(char* fileName){
+    execlp("gzip",
+           "gzip",
+           "-d"  ,
+           "-c"  ,
+           fileName,
+           NULL );
 }
