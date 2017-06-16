d60d41bc9af95b872fa8330200786c90d593fd75
Julio Renner <juliorenner123@gmail.com>
2017-06-15 22:37:50 -0300

    Update progresso1.c
    Merge das alterações

--- antivirus/src/progresso1.c
+++ antivirus/src/progresso1.c
@@ -11,108 +11,111 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <string.h>
 #include <fcntl.h>
 #include <stdbool.h>
-#include <string.h>
 
-#define _XOPEN_SOURCE 700
+#define _XOPEN_SOURCE = 700;
 #define _GNU_SOURCE
 
-char* FILE_NAME = "teste.txt.gz";
+const int SIZE = 11;
+char* signature;
 
-const int SIZE = 10;
-char* assinatura;
-
-int openFile(char* fileName);
 bool isZippedFile(int fileDescriptor);
+bool lookForVirus(int iFileDescriptor);
+int openFile(char* fileName);
 int unzipFile(char* fileName);
+bool hasVirus(char* buffer, int* verifica, int res);
 void executeUnzip(char* fileName);
-int processaArquivo(char* buffer, int verifica);
+
 
 int main(int argc, const char * argv[]) {
-	assinatura = argv[1];
 
-    int fileDescriptorToCheckType = openFile(FILE_NAME);
+    char sFile[30];
+    while( (scanf("%30[^\n]%*c",sFile) != EOF) ) {
+        signature = argv[1];
+        printf("signature: %s\n", signature);
 
-    unsigned int byte[2];
+        int fileDescriptorToCheckType = openFile(sFile);
 
-    int count = read(fileDescriptorToCheckType, &byte, 2);
+        printf("read file: %s\n", sFile);
 
-    if (count <= 0) {
-        printf("read failed\n");
-        exit(0);
-    }
+        unsigned int byte[2];
 
-    close(fileDescriptorToCheckType);
+        int count = read(fileDescriptorToCheckType, &byte, 2);
 
-    int iFileDescriptor;
+        if (count <= 0) {
+            printf("read failed\n");
+            exit(0);
+        }
 
-    if (isZippedFile(byte[0])){
+        close(fileDescriptorToCheckType);
 
-        printf("zipped file\n");
-        iFileDescriptor = unzipFile(FILE_NAME);
+        int iFileDescriptor;
 
-    } else {
+        if (isZippedFile(byte[0])){
+            printf("zipped file\n");
+            unzipFile(sFile);
+        } else {
+            iFileDescriptor = openFile(sFile);
 
-        iFileDescriptor = openFile(FILE_NAME);
-        int res = 0;
-        int offset = 0;
-
-        do{
-            char buffer[SIZE];
-            	for(int i = 0; i < SIZE; i++)
-            		buffer[i] = 0;
-
-        	res = read(iFileDescriptor, buffer, SIZE);
-
-        	if(res < 0){
-        		perror("Erro no read");
-        		return EXIT_FAILURE;
-        	}
-
-        	if(res == 0)
-        		printf("EOF\n");
-
-        	else{
-        		int processa = processaArquivo(buffer, offset);
-
-        		if (processa == 0){
-        			printf("POSSUI!!\n");
-        			offset = 0;
-        		}
-        		else if (processa > 0)
-        			offset = processa;
-        		else
-        			offset = 0;
-        	}
-
-        } while(res > 0);
-    }
+            lookForVirus(iFileDescriptor);
+        }
+    };
 
     return 0;
 }
 
-int processaArquivo(char* buffer, int offset){
-	int verifica = -1;
-	int teste = 0;
+bool lookForVirus(int iFileDescriptor){
+    int res = 0;
+    int offset = 0;
 
-	for(int i = 0, j = 0; i < strlen(buffer); i++){
-		if(buffer[i] == assinatura[j + offset]){
-			teste++;
-			j++;
-		}
-	}
+    do{
+        char buffer[SIZE];
+        for(int i = 0; i < SIZE; i++)
+            buffer[i] = 0;
 
-	if((teste == strlen(assinatura)) | (teste == strlen(assinatura) - offset))
-		verifica = 0;
-	else if((teste == 0) | (teste > strlen(assinatura)))
-		verifica = -1;
-	else
-		verifica = teste;
+        res = read(iFileDescriptor, buffer, SIZE);
 
-	return verifica;
+        if(res < 0){
+            perror("Erro no read");
+            return EXIT_FAILURE;
+        }
+
+        if(res == 0)
+            printf("EOF\n");
+        else {
+
+            if (hasVirus(buffer, &offset, res) == true){
+                printf("POSSUI!!\n");
+                break;
+            }
+        }
+
+    } while( res > 0);
 }
 
+bool hasVirus(char* buffer, int* offset, int res){
+    int j = 0;
+    for(int i = 0; i < res; i++){
+        printf("buffer: %c   signature: %c  offset: %i\n", buffer[i], signature[j + *(offset)], *(offset) );
+
+        if(buffer[i] == signature[j + *(offset)]){
+            j++;
+        } else {
+            j = 0;
+            *(offset) = 0;
+        }
+
+        if ( (j + *(offset)) == strlen(signature)){
+            return true;
+        }
+    }
+
+    *(offset) = j;
+
+    return false;
+}
 
 int openFile(char* fileName){
 
@@ -123,24 +126,8 @@ int openFile(char* fileName){
         exit(0);
     }
 
-    /*unsigned int byte[2];
-
-    int teste = read(fileDescriptor, &byte, 1);
-    int v1 = byte[0];
-    teste = read(fileDescriptor, &byte, 1);
-    int v2 = byte[0];
-    printf("%i\n", teste);
-
-    if ( byte[0] == "0x8b1f" ) {
-        printf("worked\n");
-    }
-
-    unsigned int teste1 = byte[0];
-
-    printf("%x\n", byte[0]);
-    //printf("%s\t", byte[1]);
-*/
     return fileDescriptor;
+
 }
 
 bool isZippedFile(int value) {
@@ -151,17 +138,6 @@ bool isZippedFile(int value) {
     return false;
 
 }
-
-
-void executeUnzip(char* fileName){
-    execlp("gzip",
-           "gzip",
-           "-d"  ,
-           "-c"  ,
-           fileName,
-           NULL );
-}
-
 
 int unzipFile(char* fileName){
     int pfd[2];
@@ -181,42 +157,17 @@ int unzipFile(char* fileName){
             executeUnzip(fileName);
         default:
             close(pfd[1]);
-
-            int res = 0;
-            int offset = 0;
-
-            do{
-                char buffer[SIZE];
-                	for(int i = 0; i < SIZE; i++)
-                		buffer[i] = 0;
-
-            	res = read(pfd[0], buffer, SIZE);
-
-            	if(res < 0){
-            		perror("Erro no read");
-            		return EXIT_FAILURE;
-            	}
-
-            	if(res == 0)
-            		printf("EOF\n");
-
-            	else{
-            		int processa = processaArquivo(buffer, offset);
-
-            		if (processa == 0){
-            			printf("POSSUI!!\n");
-            			offset = 0;
-            		}
-            		else if (processa > 0)
-            			offset = processa;
-            		else
-            			offset = 0;
-            	}
-
-            } while(res > 0);
-
-            return 0;
-            //read file and search for virus
+            lookForVirus(pfd[0]);
+            return EXIT_SUCCESS;
     }
 
+}
+
+void executeUnzip(char* fileName){
+    execlp("gzip",
+           "gzip",
+           "-d"  ,
+           "-c"  ,
+           fileName,
+           NULL );
 }
